ea6022ee771eb781c28fa41aa88b5f270344921a
Seshu-Kumar-MGIT <100747787+Seshu-Kumar-MGIT@users.noreply.github.com>
2022-08-16 23:37:20 +0530

    Star Pattern programs

---
+++ pattern1.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+#define sqr(x) x++ * x++
+int main()
+{
+	int row,column,no_rows;
+	printf("Enter number of rows:");
+	scanf("%d",&no_rows);
+	
+	for(row=1;row<=no_rows;row++)
+	{
+	    for(column=1;column<=row;column++)
+	    {
+	        printf("* ");
+	    }
+	    printf("\n");
+	}
+}
+
