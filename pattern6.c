1a08ecb04406a48ca6f88d3f90916219fe62834a
Seshu-Kumar-MGIT <100747787+Seshu-Kumar-MGIT@users.noreply.github.com>
2022-08-18 17:51:49 +0530

    Done 6 pattern programs Date:18-08-2022

---
+++ pattern6.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int row,column,no_rows;
+    printf("enter no of rows:");
+    scanf("%d",&no_rows);
+    /*
+    for(row=1;row<=no_rows;row++)
+    {
+        for(column=1;column<=row;column++)
+        {
+            printf("%d",row);
+        }
+        printf("\n");
+    }
+    */
+    for(row=no_rows;row>=1;row--)
+    {
+        for(column=1;column<=row;column++)
+        {
+            printf("%d",row);
+        }
+        printf("\n");
+    }
+    
+}
